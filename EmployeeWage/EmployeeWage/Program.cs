fabeda608e8f196dbe07c1320f0ea3882a023f9b
Prashanthglbkl <prashanthglbkl@gmail.com>
2021-11-19 16:59:53 +0530

    [Prashu] adding emp file

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace EmployeeWage
+namespace EmpWage
 {
-    class Program
+    class EmployeeUseCase1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWage");
+            int isPresent = 1;
+            Random random = new Random();
+            int randominput = random.Next(0, 2);
+
+            if (randominput == isPresent)
+            {
+                Console.WriteLine(" Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
+
 }
